49308a797748ee0a48694316c3ab194266c87c3c
Shaun Saker <sakershaun@gmail.com>
2020-09-23 09:22:25 +0200

    fix trades sorting

--- src/store/trades/selectors.ts
+++ src/store/trades/selectors.ts
@@ -2,4 +2,4 @@ import { ApplicationState } from '../store/reducers';
 import { sortArrayOfObjectsByKey } from '../../utils/sortArrayOfObjectsByKey';
 
 export const selectTrades = (state: ApplicationState) =>
-  sortArrayOfObjectsByKey(state.trades.data, 'closeTimestamp', true);
+  sortArrayOfObjectsByKey(state.trades.data, 'openTimestamp', true);
