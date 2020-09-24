1457f4f296955ef9f8e27ff1c8641129a4dcdd8b
Shaun Saker <sakershaun@gmail.com>
2020-09-24 15:48:07 +0200

    fix trades sorting

--- src/store/trades/selectors.ts
+++ src/store/trades/selectors.ts
@@ -1,5 +1,24 @@
 import { ApplicationState } from '../reducers';
 import { sortArrayOfObjectsByKey } from '../../utils/sortArrayOfObjectsByKey';
 
-export const selectTrades = (state: ApplicationState) =>
-  sortArrayOfObjectsByKey(state.trades.data, 'openTimestamp', true);
+export const selectTrades = (state: ApplicationState) => {
+  //   if no closeTimestamp, it's active, sort by openTimestamp and put at top of list
+  // if closeTimestamp, it's closed, sort by closeTimestamp
+  const openTrades = state.trades.data.filter((trade) => !trade.closeTimestamp);
+  const sortedOpenTrades = sortArrayOfObjectsByKey(
+    openTrades,
+    'openTimestamp',
+    true,
+  );
+  const closedTrades = state.trades.data.filter(
+    (trade) => trade.closeTimestamp,
+  );
+  const sortedClosedTrades = sortArrayOfObjectsByKey(
+    closedTrades,
+    'closeTimestamp',
+    true,
+  );
+  const sortedTrades = [...sortedOpenTrades, ...sortedClosedTrades];
+
+  return sortedTrades;
+};
