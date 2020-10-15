74f00ebb9b3f11d0213a2bfa43a540f38169b999
Shaun Saker <sakershaun@gmail.com>
2020-10-15 08:21:37 +0200

    add react-native-testing-library

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   setupFiles: ['<rootDir>/jest/setup.js'],
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
   moduleNameMapper: {
     '.+\\.(png|svg)$': 'jest-transform-stub',
   },
