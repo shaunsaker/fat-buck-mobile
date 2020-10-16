e5220936a6b052a3bd5c9b8d19a5075a5a804e06
Shaun Saker <sakershaun@gmail.com>
2020-10-16 16:17:55 +0200

    add SelectWalletSlide tests

--- src/components/Deposit/SelectWalletSlide.test.tsx
+++ src/components/Deposit/SelectWalletSlide.test.tsx
@@ -1,24 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
-import { Provider } from 'react-redux';
-import { store } from '../../store';
 import { SelectWalletSlide } from './SelectWalletSlide';
+import { mountComponent } from '../../testUtils';
 
 describe('SelectWalletSlide', () => {
-  const mountComponent = () => {
-    const mockStore = store;
-    store.dispatch = jest.fn();
-
-    const component = (
-      <Provider store={mockStore}>
-        <SelectWalletSlide />
-      </Provider>
-    );
-
-    return render(component);
-  };
-
   it('renders', () => {
-    mountComponent();
+    mountComponent(<SelectWalletSlide />);
   });
 });
