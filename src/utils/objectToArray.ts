ff386cb07d0b4d25de01d5a68e97b1c3be4c1012
Shaun Saker <sakershaun@gmail.com>
2020-10-16 16:23:24 +0200

    objectToArray works with no args

--- src/utils/objectToArray.ts
+++ src/utils/objectToArray.ts
@@ -1,6 +1,10 @@
 type Object = Record<string, any>;
 
 export const objectToArray = (object: Object): Object[] => {
+  if (!object) {
+    return [];
+  }
+
   return Object.keys(object).map((key) => ({
     ...object[key],
   }));
