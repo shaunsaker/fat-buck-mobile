f105e94d455ac175656a45309bc5cb620766e204
Shaun Saker <sakershaun@gmail.com>
2020-10-19 13:02:06 +0200

    fix moment mock

--- __mocks__/moment/index.js
+++ __mocks__/moment/index.js
@@ -1,6 +1,6 @@
 export const MOCKED_MOMENT_ISO_STRING = '2018-08-26T19:17:02.349Z';
 
 const mockedMoment = () =>
-  jest.requireActual('moment')('2020-01-01T00:00:00.000Z');
+  jest.requireActual('moment')(MOCKED_MOMENT_ISO_STRING);
 
 export default mockedMoment;
