03df0fa5d34fadee92ed5ad675667b3bc28a306b
Shaun Saker <sakershaun@gmail.com>
2020-11-17 12:57:43 +0200

    removed unused file

--- src/components/EditWallet/EditWallet.testUtils.tsx
+++
@@ -1,8 +0,0 @@
-import { EditWallet, EditWalletProps } from '.';
-import { mountComponent } from '../../testUtils/mountComponent';
-
-export const mountEditWallet = ({ props }: { props: EditWalletProps }) => {
-  const { getByText, getByPlaceholderText, queryByText, spy } = mountComponent(
-    <EditWallet {...props} />,
-  );
-};
