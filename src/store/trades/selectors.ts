811a048eee641c22bc9c2c4166a3d06744016beb
Shaun Saker <sakershaun@gmail.com>
2020-11-30 17:26:34 +0200

    fix selectTrades selector

--- src/store/trades/selectors.ts
+++ src/store/trades/selectors.ts
@@ -5,24 +5,21 @@ export const selectTrades = (
   state: ApplicationState,
   oldestToLatest?: boolean,
 ) => {
-  // if no closeTimestamp, it's active, sort by openTimestamp and put at top of list
-  // if closeTimestamp, it's closed, sort by closeTimestamp
-  const tradesArray = Object.keys(state.trades.data).map(
-    (id) => state.trades.data[id],
-  );
-  const openTrades = tradesArray.filter((trade) => !trade.closeTimestamp);
-  const sortedOpenTrades = sortArrayOfObjectsByKey(
-    openTrades,
-    'openTimestamp',
-    true,
-  );
-  const closedTrades = tradesArray.filter((trade) => trade.closeTimestamp);
-  const sortedClosedTrades = sortArrayOfObjectsByKey(
-    closedTrades,
-    'closeTimestamp',
+  // closed trades have a closeTimestamp while active trades don't
+  // save a _date for sorting
+  const tradesArray = Object.keys(state.trades.data).map((id) => ({
+    ...state.trades.data[id],
+    _date:
+      state.trades.data[id].closeTimestamp ||
+      state.trades.data[id].openTimestamp,
+  }));
+
+  // sort by internal _date
+  const sortedTrades = sortArrayOfObjectsByKey(
+    tradesArray,
+    '_date',
     oldestToLatest ? false : true,
   );
-  const sortedTrades = [...sortedOpenTrades, ...sortedClosedTrades];
 
   return sortedTrades;
 };
