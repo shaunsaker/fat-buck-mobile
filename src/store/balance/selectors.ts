4ed5f2448997353781299578dc061cf83b1330de
Shaun Saker <sakershaun@gmail.com>
2020-12-06 08:29:30 +0200

    fix selectBTCPrice

--- src/store/balance/selectors.ts
+++ src/store/balance/selectors.ts
@@ -13,10 +13,19 @@ export const selectBalance = (state: ApplicationState) => {
 };
 
 export const selectBTCPrice = (state: ApplicationState) => {
+  if (
+    !state.balance.data.amount ||
+    !state.balance.data.amount ||
+    !state.currency.rate
+  ) {
+    return '0.00';
+  }
+
   const roundedBTCPrice = getFloatString(
     (state.balance.data.value * state.currency.rate) /
       state.balance.data.amount,
   );
+
   return roundedBTCPrice;
 };
 
